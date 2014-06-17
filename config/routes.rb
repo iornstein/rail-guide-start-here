5877a9d06eb2196693266ae594b692e5458fcfbf
John Ryan and Mariana Lenetis <pair+john+mariana@pivotallabs.com>
2014-06-17 15:25:23 -0700

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
   #get "welcome/index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
